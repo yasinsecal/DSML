3b6905edd31a62fdc66ed4ed76ab76e21dfedfe3
yasinsecal <yasin.secal@boun.edu.tr>
2024-08-26 23:24:22 +0300

    add_features functions

--- helper_functions.py
+++ helper_functions.py
@@ -7,3 +7,22 @@ def df_null_summary(df: pd.DataFrame) -> pd.DataFrame:
     info = info.append(pd.DataFrame(df.isnull().sum()/df.shape[0]*100).T.rename(index = {0:'null values{%}'}))
     return info
 
+
+def df_add_features(df:pd.DataFrame) -> pd.DataFrame:
+
+    """
+    Creates time series features from datetime index.
+    """
+
+    df = df.copy()
+    df['date'] = pd.to_datetime(df['date'])
+    df['hour'] = df['date'].dt.hour
+    df['dayofweek'] = df['date'].dt.dayofweek
+    df['quarter'] = df['date'].dt.quarter
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+    df['dayofyear'] = df['date'].dt.dayofyear
+    df['dayofmonth'] = df['date'].dt.day
+    df['weekofyear'] = df['date'].dt.weekofyear
+
+    return df
